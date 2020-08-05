29e1b980d41451ce6d3182bc5e0450758c84748f
hewittwatkins <hewittwatkins@localhost>
2020-08-05 15:26:24 -0700

    Learned how the GUI works a bit and started experimenting around with it.

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,16 @@
 package intro_to_array_lists;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class GuestBook {
+	
+	public static void main(String[] args) {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +19,30 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+		
+	// JFrame
+		
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton();
+	JTextField text = new JTextField(20);
+	JLabel label = new JLabel();
+	
+	frame.setVisible(true);
+	frame.setLocationRelativeTo(null);
+	frame.setSize(500,500);
+	label.setText("Hellooo");
+	
+	panel.add(button1);
+	panel.add(text);
+	panel.add(label);
+	panel.setBackground(Color.cyan);
+	
+	
+	// ActionLister --> learn what is next class from the teacher / ask them about it
+	
+	frame.add(panel);
+
+	}
 }
+	
